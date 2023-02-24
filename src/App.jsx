14007a025b8bc82547e3789f48dba125e2678613
Pedro Miguel Barbosa <pedro.miguel.barbosa@celfocus.com>
2023-02-24 18:08:47 +0000

    Components placing and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import "./App.scss";
 import Paragraph from "./components/atoms/Paragraph/paragraph";
+import Heading from "./components/atoms/Heading/Heading";
+import Button from "./components/atoms/Button/Button";
+import AreasScored from "./components/molecules/AreasScored/AreasScored";
+import "./assets/styles/main.scss";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Paragraph text={'Your Result'} />     
-      76 of 100 
-      Great
+    <div className="app">
+      <section className="score flex flex__align--center flex--column py--16 px--64">
+        <Paragraph text={"Your Result"} />
+        76 of 100 
+        <Heading className="color-primary--white" type="h1" text={'Great'} />
+        <Paragraph className="mt-0"
+          text={
+            "You scored higher than 65% of the people who have taken these tests."
+          }
+        />
+      </section>
 
-      <Paragraph text={'You scored higher than 65% of the people who have taken these tests.'} /> 
-      Summary 
-      Reaction 80 / 100 Memory 92 / 100 Verbal
-      61 / 100 Visual 72 / 100 Continue
+      <section className="summary p--32">
+        <Heading type="h2" text={'Summary'} />
+        <AreasScored svg='reaction' text='Reaction' score='80 / 100'/>
+        <AreasScored svg='memory' text='Memory' score='92 / 100'/>
+        <AreasScored svg='verbal' text='Verbal' score='61 / 100'/>
+        <AreasScored svg='visual' text='Visual' score='72 / 100'/>
+        <Button text={'Continue'}></Button>
+      </section>
 
-      
     </div>
   );
 }
--- src/components/atoms/Heading/Heading.jsx
+++ src/components/atoms/Heading/Heading.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Heading = () => {
-  return (
-    <div>Heading</div>
-  )
+const Heading = ({type, text}) => {
+
+  switch (type) {
+    case 'h1':
+      return <h1>{text}</h1>;
+    case 'h2':
+      return <h2>{text}</h2>;
+    case 'h3':
+      return <h3>{text}</h3>;
+    case 'h4':
+      return <h4>{text}</h4>;
+    case 'h5':
+      return <h5>{text}</h5>;
+    case 'h6':
+      return <h6>{text}</h6>;
+    default:
+      return null;
+  }
 }
 
-export default Heading
+export default Heading;
