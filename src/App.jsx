7b16bc05d92daf96362e0e6b417c402518cfa97a
Pedro Miguel Barbosa <pedro.miguel.barbosa@celfocus.com>
2023-03-03 11:59:42 +0000

    Desktop design finished

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className="app">
       <section className="score flex flex__align--center flex--column py--16 px--64">
-        <Paragraph className="score__yourresult" text={"Your Result"} />
+        <Paragraph className="score__yourresult color-gradient--lightslateblue2" text={"Your Result"} />
         <ScoreBadge className='my--4' />
         <Heading className="mt--16 mb--8 color-primary--white" type="h1" text={'Great'} />
-        <Paragraph className="mt--0 mb--24 color-gradient--lightslateblue2"
+        <Paragraph className="score__text mt--0 mb--24 color-gradient--lightslateblue2"
           text={
             "You scored higher than 65% of the people who have taken these tests."
           }
@@ -28,8 +28,9 @@ function App() {
         <AreasScored className='bgcolor-lightblue' svg='visual' text='Visual' score='72'/>
         <Button className="flex__justify--center bgcolor-darkblue color-primary--white border-radius--30 py--16 mt--8" text={'Continue'}></Button>
       </section>
-
+      
       <Footer className="mb--12" />
+      
     </div>
   );
 }
--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = ({className}) => {
   return (
     <footer className={className}>
         Challenge by {" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
           Frontend Mentor
         </a>
         . Coded by <a href="https://github.com/pedrommb7">Pedro Barbosa</a>.
